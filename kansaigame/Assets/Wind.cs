cb8e30bd6b7b8b368f83258c8e1665156bea8010
takashima_ryo <takashima@cloverlab.jp>
2015-11-25 01:45:28 +0900

    一旦PUSH

--- kansaigame/Assets/Wind.cs
+++ kansaigame/Assets/Wind.cs
@@ -18,24 +18,24 @@ public class Wind : MonoBehaviour {
 	}
 
 
-	public float speed = 6.0f;
+	public float speed = 0.4f;
 
 	public void Update()
 	{
 		// 上下に対応する入力による上下移動
 //		transform.GetComponent<Rigidbody>().velocity = new Vector3(0, Input.GetAxisRaw("Vertical") * speed, 0);
 
-		if (Input.GetKey(KeyCode.UpArrow)){
-			transform.Translate(transform.forward * 1);
+		if (Input.GetKey(KeyCode.W)){
+			transform.Translate(new Vector3(0, 0, 1*0.1f));
 		}
-		if (Input.GetKey(KeyCode.DownArrow)){
-			transform.Translate(transform.forward * -1);
+		if (Input.GetKey(KeyCode.E)){
+			transform.Translate(new Vector3(0, 0, -1));
 		}
-		if (Input.GetKey(KeyCode.RightArrow)){
-			transform.Translate(transform.right * 1);
+		if (Input.GetKey(KeyCode.R)){
+			transform.Translate(new Vector3(1, 0, 0));
 		}
-		if (Input.GetKey(KeyCode.LeftArrow)){
-			transform.Translate(transform.right * -1);
+		if (Input.GetKey(KeyCode.Q)){
+			transform.Translate(new Vector3(-1, 0, 0));
 		}
 	}
 }
